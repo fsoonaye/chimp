dfd596357a3baf9b7e2e7c8744c2257683c76a33
gameskun <florian@soonaye.fr>
2025-03-09 21:14:57 +0100

    added bench support for OpenBench

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include "../include/chess.hpp"
 #include "uci.h"
+#include "bench.h"
 
-int main() {
+int main(int argc, char* argv[]) {
+    if (argc > 1 && std::string(argv[1]) == "bench")
+    {
+        run_bench();
+        return 0;
+    }
+
     UCIEngine uci;
     uci.loop();
 
