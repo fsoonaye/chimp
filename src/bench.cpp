808ea88060aead9ef8f3d4e22bd86414eea9cfc8
gameskun <florian@soonaye.fr>
2025-03-12 12:13:16 +0100

    Implemented proper benchmarking for openbench standards

---
+++ src/bench.cpp
@@ -0,0 +1,41 @@
+#include "bench.h"
+#include "types.h"
+#include "engine.h"
+
+#include "perft.h"
+
+
+int bench::run(int depth) {
+    uint64_t nodes = 0;
+
+    Limits limits;
+    limits.depth = depth;
+    limits.nodes = 0;
+    limits.time  = Time();
+
+    int i = 1;
+
+    auto t0 = std::chrono::high_resolution_clock::now();
+
+    for (auto& fen : benchfens)
+    {
+        std::cout << "\nPosition: " << i++ << "/" << benchfens.size() << " " << fen << std::endl;
+
+        Engine engine;
+        engine.limits = limits;
+        engine.board.setFen(fen);
+        engine.get_bestmove(depth);
+
+        nodes += engine.nodes;
+    }
+
+    auto t1      = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count();
+
+    auto nps = signed((nodes / (elapsed + 1)) * 1000);
+
+    std::cout << "info string " << elapsed / 1000.0 << " seconds" << std::endl;
+    std::cout << "\n" << nodes << " nodes " << nps << " nps" << std::endl;
+
+    return 0;
+}
