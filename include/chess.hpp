152abb02b6cf3b5e57f5081bbe09e84257be33f9
gameskun <florian@soonaye.fr>
2025-03-18 17:45:39 +0100

    !! changed score attribute in Move class from int16_t to int to handle large score numbers

--- include/chess.hpp
+++ include/chess.hpp
@@ -1310,7 +1310,6 @@ class Piece {
 }  // namespace chess
 
 namespace chess {
-
 class Move {
    public:
     Move() = default;
@@ -1319,13 +1318,13 @@ class Move {
         score_(0) {}
 
     /**
-     * @brief Creates a move from a source and target square.
-     * @tparam MoveType
-     * @param source
-     * @param target
-     * @param pt leave this empty if it is not a promotion move, otherwise pass the piece type of the new piece.
-     * @return
-     */
+             * @brief Creates a move from a source and target square.
+             * @tparam MoveType
+             * @param source
+             * @param target
+             * @param pt leave this empty if it is not a promotion move, otherwise pass the piece type of the new piece.
+             * @return
+             */
     template<std::uint16_t MoveType = 0>
     [[nodiscard]] static constexpr Move
     make(Square source, Square target, PieceType pt = PieceType::KNIGHT) noexcept {
@@ -1338,44 +1337,44 @@ class Move {
     }
 
     /**
-     * @brief Get the source square of the move.
-     * @return
-     */
+             * @brief Get the source square of the move.
+             * @return
+             */
     [[nodiscard]] constexpr Square from() const noexcept {
         return static_cast<Square>((move_ >> 6) & 0x3F);
     }
 
     /**
-     * @brief Get the target square of the move.
-     * @return
-     */
+             * @brief Get the target square of the move.
+             * @return
+             */
     [[nodiscard]] constexpr Square to() const noexcept { return static_cast<Square>(move_ & 0x3F); }
 
     /**
-     * @brief Get the type of the move. Can be NORMAL, PROMOTION, ENPASSANT or CASTLING.
-     * @return
-     */
+             * @brief Get the type of the move. Can be NORMAL, PROMOTION, ENPASSANT or CASTLING.
+             * @return
+             */
     [[nodiscard]] constexpr std::uint16_t typeOf() const noexcept {
         return static_cast<std::uint16_t>(move_ & (3 << 14));
     }
 
     /**
-     * @brief Get the promotion piece of the move, should only be used if typeOf() returns PROMOTION.
-     * @return
-     */
+             * @brief Get the promotion piece of the move, should only be used if typeOf() returns PROMOTION.
+             * @return
+             */
     [[nodiscard]] constexpr PieceType promotionType() const noexcept {
         return static_cast<PieceType::underlying>(((move_ >> 12) & 3)
                                                   + PieceType(PieceType::KNIGHT));
     }
 
     /**
-     * @brief Set the score for a move. Useful if you later want to sort the moves.
-     * @param score
-     */
-    constexpr void setScore(std::int16_t score) noexcept { score_ = score; }
+             * @brief Set the score for a move. Useful if you later want to sort the moves.
+             * @param score
+             */
+    constexpr void setScore(int score) noexcept { score_ = score; }
 
     [[nodiscard]] constexpr std::uint16_t move() const noexcept { return move_; }
-    [[nodiscard]] constexpr std::int16_t  score() const noexcept { return score_; }
+    [[nodiscard]] constexpr int           score() const noexcept { return score_; }
 
     constexpr bool operator==(const Move& rhs) const noexcept { return move_ == rhs.move_; }
     constexpr bool operator!=(const Move& rhs) const noexcept { return move_ != rhs.move_; }
@@ -1389,7 +1388,7 @@ class Move {
 
    private:
     std::uint16_t move_;
-    std::int16_t  score_;
+    int           score_;  // Changed from std::int16_t to int
 };
 
 inline std::ostream& operator<<(std::ostream& os, const Move& move) {
