5f8dc2523d8d7c7d558adb1fec9ab517e77b4064
gameskun <florian@soonaye.fr>
2025-03-30 15:10:42 +0200

    added go eval and go mate UCI support

--- src/uci.h
+++ src/uci.h
@@ -8,9 +8,12 @@ using namespace chess;
 class UCIEngine {
    public:
     void loop();
+    void print_engine_info();
     void position(std::istringstream& is);
     void go(std::istringstream& is);
+    void eval();
+
+    Engine engine;
 
    private:
-    Engine engine;
 };
