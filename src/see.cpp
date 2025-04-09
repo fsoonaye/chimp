1ab56002725cb33508ee32ecc2da4e6c4d5774bf
gameskun <florian@soonaye.fr>
2025-04-09 19:20:07 +0200

    fix: finding the least valuable attacker

--- src/see.cpp
+++ src/see.cpp
@@ -1,16 +1,19 @@
 #include "see.h"
 
+
+
 bool SEE(Board board, Move move, int treshold) {
     Square to   = move.to();
     Square from = move.from();
+    const Color initiating_side = board.at<Piece>(from).color();
 
-    int swap = SEEvalues[static_cast<int>(board.at<PieceType>(to))] - treshold;
+    int swap = SEEvalues[board.at<PieceType>(to)] - treshold;
 
     // If even before subtracting our own piece’s cost, the swap is losing, then refuse
     if (swap < 0)
         return false;
 
-    swap -= SEEvalues[static_cast<int>(board.at<PieceType>(from))];
+    swap -= SEEvalues[board.at<PieceType>(from)];
 
     // If the exchange is still winning after this capture, then accept
     if (swap >= 0)
@@ -29,7 +32,7 @@ bool SEE(Board board, Move move, int treshold) {
     Bitboard orthogonal_sliders = board.pieces(PieceType::ROOK) | queens;
 
     // Determine whose turn it is next in the exchange sequence.
-    Color side = ~board.at<Piece>(from).color();
+    Color side = ~initiating_side;
 
     // Loop through potential captures until no attacker is available.
     while (true)
@@ -40,49 +43,54 @@ bool SEE(Board board, Move move, int treshold) {
         // Determine the set of attackers for the side to move now.
         Bitboard my_attackers = attackers & board.us(side);
 
-        if (!my_attackers)
+        if (my_attackers.empty())
             break;
 
-        PieceType::underlying pt = PieceType::PAWN;
-        for (auto candidate : {PieceType::PAWN, PieceType::KNIGHT, PieceType::BISHOP,
-                               PieceType::ROOK, PieceType::QUEEN, PieceType::KING})
-        {
-            if (my_attackers & board.pieces(pt))
-            {
-                pt = candidate;
-                break;
-            }
-        }
+        // Finding the least valuable attacker
+        PieceType pt = PieceType::NONE;
+        if (my_attackers & board.pieces(PieceType::PAWN))
+            pt = PieceType::PAWN;
+        else if (my_attackers & board.pieces(PieceType::KNIGHT))
+            pt = PieceType::KNIGHT;
+        else if (my_attackers & board.pieces(PieceType::BISHOP))
+            pt = PieceType::BISHOP;
+        else if (my_attackers & board.pieces(PieceType::ROOK))
+            pt = PieceType::ROOK;
+        else if (my_attackers & board.pieces(PieceType::QUEEN))
+            pt = PieceType::QUEEN;
+        else if (my_attackers & board.pieces(PieceType::KING))
+            pt = PieceType::KING;
+        assert (pt != PieceType::NONE);
 
         // alternate who is capturing next
         side = ~side;
 
         // Negamax the swap value
-        swap = -swap - 1 - SEEvalues[static_cast<int>(pt)];
+        swap = -swap - 1 - SEEvalues[pt];
 
         // if the new swap indicates a winning net results, then accept
         if (swap >= 0)
         {
-
-            if (pt == PieceType::KING && (attackers & board.us(side)))
+            if ((pt == PieceType::KING) && (attackers & board.us(side)))
                 side = ~side;
 
             break;
         }
 
         // remove the captured piece from the board
-        occupied ^= (my_attackers & board.pieces(pt)).lsb();
+        int lsb_index = (my_attackers & board.pieces(pt)).lsb();
+        occupied.clear(lsb_index);
 
 
         // update the set of attackers by adding discovered attackers (x-ray)
         if (pt == PieceType::PAWN || pt == PieceType::BISHOP || pt == PieceType::QUEEN)
-            attackers |= attacks::bishop(to, occupied) & diagonal_sliders;
+            attackers |= (attacks::bishop(to, occupied) & diagonal_sliders);
 
         if (pt == PieceType::ROOK || pt == PieceType::QUEEN)
-            attackers |= attacks::rook(to, occupied) & orthogonal_sliders;
+            attackers |= (attacks::rook(to, occupied) & orthogonal_sliders);
     }
 
     // If the final side to move is NOT the same as the color of the original moving piece,
     // then the initiating side wins the exchange.
-    return side != board.at<Piece>(from).color();
+    return (side != initiating_side);
 }
