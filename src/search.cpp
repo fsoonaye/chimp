61db8079bcb696b1d16ad290ac85e41b20181e22
gameskun <florian@soonaye.fr>
2025-04-12 02:39:16 +0200

    feat(search): implement late move reduction

--- src/search.cpp
+++ src/search.cpp
@@ -95,6 +95,7 @@ int Engine::negamax_search(int alpha, int beta, int depth, int ply) {
     int  bestscore = -VALUE_INF;
     Move bestmove  = Move::NO_MOVE;
     Move move      = Move::NO_MOVE;
+    int  movecount = 0;
     int  score;
 
     // generating legal moves
@@ -102,27 +103,56 @@ int Engine::negamax_search(int alpha, int beta, int depth, int ply) {
     movegen::legalmoves(moves, board);
 
     // check for checkmate or stalemate
+    bool is_in_check = board.inCheck();
     if (moves.empty())
-        return board.inCheck() ? mated_in(ply) : 0;
+        return is_in_check ? mated_in(ply) : 0;
 
     MovePicker mp(*this, moves, ttmove, ply);
     while ((move = mp.next_move()) != Move::NO_MOVE)
     {
+        movecount++;
+
+        bool go_full_depth = true;
+        bool is_capture    = board.isCapture(move);
+        int  newdepth      = depth - 1;
+
+
+        // clang-format off
+        if (depth >= 3 && movecount > 3 &&
+            !is_root_node && !is_in_check && !is_pv_node && 
+            move.typeOf() != Move::PROMOTION && !is_capture)
+        {  // clang-format on
+            int reduction = 1 + std::log(depth) * std::log(movecount) / 3;
+            newdepth      = std::max(1, depth - 1 - reduction);
+            go_full_depth = false;
+        }
+
         board.makeMove(move);
 
-        // Principal Variation Search
-        if (is_root_node)
-            // For the first move → full window search.
-            score = -negamax_search<nodetype>(-beta, -alpha, depth - 1, ply + 1);
+        // Search with reduced depth if LMR applies
+        if (!go_full_depth)
+        {
+            score = -negamax_search<NON_PV>(-alpha - 1, -alpha, newdepth, ply + 1);
 
+            if (score > alpha)
+                go_full_depth = true;
+        }
         else
         {
-            // First, search with a null window [-α-1, -α].
-            score = -negamax_search<NON_PV>(-alpha - 1, -alpha, depth - 1, ply + 1);
+            // Principal Variation Search
+            if (is_root_node)
+                // For the first move → full window search.
+                score = -negamax_search<nodetype>(-beta, -alpha, newdepth, ply + 1);
 
-            // If the score ∈ [α, β], it might be better than α, so re-search with full window.
-            if (score > alpha && score < beta && is_pv_node)
-                score = -negamax_search<PV>(-beta, -alpha, depth - 1, ply + 1);
+            else
+            {
+                // First, search with a null window [-α-1, -α].
+                score = -negamax_search<NON_PV>(-alpha - 1, -alpha, newdepth, ply + 1);
+
+                // If the score ∈ [α, β], it might be better than α, so re-search with full window.
+                if (score > alpha && score < beta && is_pv_node)
+                    score = -negamax_search<PV>(-beta, -alpha, newdepth, ply + 1);
+            }
         }
 
         board.unmakeMove(move);
@@ -151,7 +181,7 @@ int Engine::negamax_search(int alpha, int beta, int depth, int ply) {
         if (score >= beta)
         {
             // Store killer moves
-            if (move != killer_moves[ply][0] && !board.isCapture(move))
+            if (move != killer_moves[ply][0] && !is_capture)
             {
                 // Shift the previous killer and store the new one
                 killer_moves[ply][1] = killer_moves[ply][0];
